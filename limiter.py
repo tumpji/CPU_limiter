fbea2d8ba18457cf551c3c570714d8883c8025ff
Jiri Tumpach <tumpji@gmail.com>
2019-07-17 23:17:28 +0200

    some git error

--- limiter.py
+++ limiter.py
@@ -22,12 +22,13 @@ import os
 import time
 import psutil
 import argparse
+import collections
 
-MAX_OVERTIME_PERC = 0.98    # maximum overtime allowed
-MIN_OVERTIME_PERC = 0.96    # end limiting processes by this % overtime
+MAX_OVERTIME_PERC = 0.70    # maximum overtime allowed
+MIN_OVERTIME_PERC = 0.50    # end limiting processes by this % overtime
 CHECK_EVERY = 10            # if not stopped it will check every [sec] if is not overtime
 
-SLEEP_TIME_THREASHOLD = 0.05    # if automatic selection of processes, this is minimum proportion of cpu time to select a process
+SLEEP_TIME_THREASHOLD = 0.10    # if automatic selection of processes, this is minimum proportion of cpu time to select a process
 SLEEP_TIME = 1                  # time in sleep until it will check again [sec]
 WAKE_TIME = 0.2                 # when renew, it will be runing this [sec]
 
@@ -39,44 +40,59 @@ assert SLEEP_TIME > 0
 assert SLEEP_TIME_THREASHOLD > 0.
 
 class Locker:
-    def __init__(self, allocated_cpus, pids=None, regexp=None):
-        self.list_of_stopped_processes = []
+    def __init__(self, allocated_cpus, from_now, pids=None, regexp=None):
         self.allocated_cpus = allocated_cpus
-        self.verbose = verbose
-
+        self.from_now = from_now
         self.pids = pids
         self.regexp = regexp
 
+        self.started_time = time.time() if from_now else psutil.users()[0].started
+        self.list_of_stopped_processes = []
+
+        # set up initial state
+        self.state_in_start = collections.defaultdict(float)
+        for proc in self._process_iter():
+            self.state_in_start[proc['pid']] = proc['time']
+
+    '''
+    def _init_time(self):
+        """ return initial time -> user log in / start of script """
+        return psutil.users()[0].started if not self.from_now else self.started_time
+    '''
+
     def _threshold_process_time(self):
-        started = psutil.users()[0].started
-        passed = (time.time() - started)
-        threashold = passed * SLEEP_TIME_THREASHOLD
-        return threashold
+        """ returns limit from which is process targeted in automatic detection """
+        return (time.time() - self.started_time) * SLEEP_TIME_THREASHOLD
 
     def _process_iter(self):
+        """ iterate through all $USER process """
         username = psutil.users()[0].name
         for proc in psutil.process_iter(attrs=[
             'pid', 'cpu_times', 'username', 'name']):
             proc = proc.info
             if proc['username'] == username:
                 proc['time'] = proc['cpu_times'].user + proc['cpu_times'].system
+
+                if proc['time'] < self.state_in_start[proc['pid']]:
+                    print('Cleaning PID {}'.format(proc['pid']))
+                    self.state_in_start[proc['pid']] = 0.
+
+                proc['time'] -= self.state_in_start[proc['pid']]
+
                 yield proc
 
     def _process_filter(self, input):
-        threshold_process_time = self._threshold_process_time(started)
-
+        threshold_process_time = self._threshold_process_time()
         for proc in input:
             if proc['time'] >= threshold_process_time:
                 yield proc
 
     def overtime(self, MIN=False):
-        user_info = psutil.users()[0]
-        maxtime = (time.time() - user_info.started) * self.allocated_cpus
+        maxtime = (time.time() - self.started_time) * self.allocated_cpus
 
         sum = 0
         for proc in self._process_iter():
             sum += proc['time']
-
         if MIN: return maxtime * MIN_OVERTIME_PERC <= sum
         else: return maxtime * MAX_OVERTIME_PERC <= sum
 
@@ -87,9 +103,10 @@ class Locker:
         mypid = os.getpid()
 
         r = []
-        for proc in self.process_filter(self._process_iter()):
+        for proc in self._process_filter(self._process_iter()):
             if proc['pid'] == mypid:
                 continue
+            print(proc['name'], proc['time'], self._threshold_process_time())
             r.append(proc['pid'])
         return r
 
@@ -103,16 +120,19 @@ class Locker:
             self._run_process(i)
 
     def _stop_process(self, pid):
+        """ stop process by pid """
         print("Stopping {} ...".format(pid))
-        os.kill(pid, signal.SIGSTOP)
+        #os.kill(pid, signal.SIGSTOP)
 
     def _run_process(self, pid):
+        """ returns to its runing state (by pid)"""
         print("Running {} ...".format(pid))
-        os.kill(pid, signal.SIGCONT)
+        #os.kill(pid, signal.SIGCONT)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--from_now', action='store_true')
     parser.add_argument('--ncpus', type=int, default=None, help='number of cores given')
     parser.add_argument('--renew', action='store_true', help='programs may be more safe: pauses program only for limited amount of time')
 
@@ -131,24 +151,26 @@ if __name__ == '__main__':
             print('Warning: NCPUS is not provided assuming it is {}'.format(args.ncpus))
             print('\tBased on number of CPUS in system')
 
-    while True:
-        l = Locker(args.ncpus, pids=args.pids, regexp=args.regexp)
+    locker = Locker(args.ncpus, from_now=args.from_now, pids=args.pids, regexp=args.regexp)
 
-        if l.overtime():
+    while True:
+        time.sleep(CHECK_EVERY)
+        print('Check')
+
+        if locker.overtime():
             if args.renew:
-                while l.overtime(MIN=True):
-                    l.stop()
+                while locker.overtime(MIN=True):
+                    locker.stop()
                     time.sleep(SLEEP_TIME) 
-                    l.run()
+                    locker.run()
                     time.sleep(WAKE_TIME)
             else:
-                l.stop()
+                locker.stop()
                 time.sleep(SLEEP_TIME)
 
-                while l.overtime(MIN=True):
+                while locker.overtime(MIN=True):
                     time.sleep(SLEEP_TIME)
-                l.run()
-        time.sleep(CHECK_EVERY)
+                locker.run()
             
 
 
